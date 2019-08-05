2206953ad4c684117f0b1e8bf3cc687187b64e4f
Yevhen_Tataryn <Yevhen_Tataryn@EPPLGDAW0533>
2019-08-05 12:16:04 +0200

    added routes definition

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 console.log('path: ', window.location.pathname); // can be removed
 
+// Routes:
+//   '' - gallery page
+//   '/' - gallery page
+//   '/images' - gallery page
+//   'images/:id' - sigle image page
+//   'create' - create page
+
 window.addEventListener('DOMContentLoaded', (event) => {
 
   // TODO: your code is here
