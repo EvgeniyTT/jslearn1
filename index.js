c6189e71bfd1b32af3793586735175757fee0e2e
Yevhen_Tataryn <Yevhen_Tataryn@EPPLGDAW0533>
2019-08-05 11:46:17 +0200

    added todos

--- index.js
+++ index.js
@@ -1,33 +1,7 @@
-// console.log('uuid: ', uuidv4());
-console.log('hey man!');
-console.log(window.location.pathname);
+console.log('path: ', window.location.pathname); // can be removed
 
-const route = window.location.pathname.split('/').pop();
-const homeBlock = document.querySelector('#home');
-const galleryBlock = document.querySelector('#gallery');
-const imageBlock = document.querySelector('#image');
+window.addEventListener('DOMContentLoaded', (event) => {
 
-console.log(route)
-if (route === '' || route === '/' || route === 'home') {
-  // show home
-  console.log('homeBlock: ', homeBlock);
-  homeBlock.style = 'display: block';
-}
+  // TODO: your code is here
 
-fetch('http://localhost:3000/images')
-  .then(res => res.json())
-  .then(images => { 
-    console.log(images);
-    // const div = document.createElement('div');
-    images.forEach(image => {
-      const img = document.createElement('img');
-      img.setAttribute('src', image.src);
-      document.body.appendChild(img);
-    });
-
-  });
-
-window.onpopstate = () => {
-  console.log(window.location.pathname);
-  // contentDiv.innerHTML = routes[window.location.pathname];
-}
+});
